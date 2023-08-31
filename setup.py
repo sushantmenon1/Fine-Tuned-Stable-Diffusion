7d07ac8e932d81b541dd1a64e2dbb1d8cbf78328
Sushant Menon <sushantmenon1@gmail.com>
2023-08-31 06:04:23 +0000

    replace google cloud python api with curl call

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import subprocess
+
 from setuptools import setup, find_packages
 from setuptools.command.install import install
 from distutils.sysconfig import get_python_lib
@@ -6,40 +8,56 @@ from pathlib import Path
 class PostInstall(install):
     def run(self):
         install.run(self)
-        download_models()
+        download_modelsv2()
 
-def download_models():
-    from google.cloud import storage
-    from google.cloud.storage.blob import Blob
-
-    # GCP bucket details
-    bucket_name = Path('playground-sushant-eefk')
-    prefix = Path('custom model')
-    blobs = ['kerala.bin', 'pytorch_custom_diffusion_weights.bin']
-    
+def download_modelsv2():
     # create model directory
     model_dir = Path(get_python_lib()).joinpath('generate').joinpath('model')
     model_dir.mkdir(parents=True, exist_ok=True)
+
+    # download weights
+    subprocess.run(['curl', 
+                    'https://storage.googleapis.com/playground-sushant-eefk/custom%20model/pytorch_custom_diffusion_weights.bin', 
+                    '-o', 
+                    model_dir.joinpath('pytorch_custom_diffusion_weights.bin')])
+
+    # download encodings
+    subprocess.run(['curl', 
+                    'https://storage.googleapis.com/playground-sushant-eefk/custom%20model/kerala.bin', 
+                    '-o', 
+                    model_dir.joinpath('kerala.bin')])
+
+# def download_models():
+#     from google.cloud import storage
+#     from google.cloud.storage.blob import Blob
+
+#     # GCP bucket details
+#     bucket_name = Path('playground-sushant-eefk')
+#     prefix = Path('custom model')
+#     blobs = ['kerala.bin', 'pytorch_custom_diffusion_weights.bin']
     
-    # initialize
-    storage_client = storage.Client()
-    bucket = storage_client.bucket(str(bucket_name))
-    blob_names = [prefix.joinpath('kerala.bin'),
-                  prefix.joinpath('pytorch_custom_diffusion_weights.bin')]
+#     # create model directory
+#     model_dir = Path(get_python_lib()).joinpath('generate').joinpath('model')
+#     model_dir.mkdir(parents=True, exist_ok=True)
     
-    # download
-    for blob_name in blob_names:
-        blob = Blob(str(blob_name), bucket)
-        file_name = blob.name.split("/")[-1]
-        print('Starting download')
-        print(model_dir)
-        blob.download_to_filename(model_dir.joinpath(file_name)) 
+#     # initialize
+#     storage_client = storage.Client()
+#     bucket = storage_client.bucket(str(bucket_name))
+#     blob_names = [prefix.joinpath('kerala.bin'),
+#                   prefix.joinpath('pytorch_custom_diffusion_weights.bin')]
+    
+#     # download
+#     for blob_name in blob_names:
+#         blob = Blob(str(blob_name), bucket)
+#         file_name = blob.name.split("/")[-1]
+#         print('Starting download')
+#         print(model_dir)
+#         blob.download_to_filename(model_dir.joinpath(file_name)) 
 
 setup(name='generate', 
       version='0.1.0',
       author='Sushant Menon',
       author_email = "sushantmenon1@gmail.com",
-      setup_requires=['google-cloud-storage'],
       install_requires=["torch", 
                         "numpy",
                         "diffusers @ git+https://github.com/sushantmenon1/diffusers.git",
